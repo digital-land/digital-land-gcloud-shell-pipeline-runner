901ddb9d33539f0c817a5e8c51c8896e76bdcc28
Rico Farina <henrique.farina@madetech.com>
2022-07-25 09:58:11 +0100

    remove workaround that is no longer needed

--- 01-package-clone-and-general-setup.sh
+++ 01-package-clone-and-general-setup.sh
@@ -1,11 +1,6 @@
 # clone digital land python
 cd $home
 git clone https://github.com/digital-land/digital-land-python.git
-# TEMPORARY: next step gets version of cli.py that surfaces '--custom-temp-dir' parameter to the digital-land pipeline command
-#   in the future this change should be merged and next step not needed.
-curl -qfsL 'https://raw.githubusercontent.com/digital-land/digital-land-python/bf8255ce99e04ff7c1f9c7c2fa6c1a4c712588aa/digital_land/cli.py' > digital-land-python/digital_land/cli.py
-
-
 echo '-------- Clone step completed'
 
 # install pipenv
